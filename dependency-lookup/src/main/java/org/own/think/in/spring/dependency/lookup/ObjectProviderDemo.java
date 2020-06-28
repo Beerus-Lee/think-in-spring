7d2d3950957bc1b88d76e06126f2dd526581a7e7
lichao44 <lichao44@58.com>
2020-06-28 18:19:30 +0800

    add

---
+++ dependency-lookup/src/main/java/org/own/think/in/spring/dependency/lookup/ObjectProviderDemo.java
@@ -0,0 +1,9 @@
+package org.own.think.in.spring.dependency.lookup;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ObjectProviderDemo {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext configApplicationContext = new AnnotationConfigApplicationContext();
+    }
+}
